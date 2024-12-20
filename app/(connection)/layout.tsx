717418c792e1853c31eda19878fc95bbd7791c16
Hilary29 <keubouhilary@gmail.com>
2024-12-20 12:59:18 +0100

    authentification

--- app/(connection)/layout.tsx
+++ app/(connection)/layout.tsx
@@ -1,23 +1,24 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+import Image from "next/image";
+import logo from "../../public/images/logo2.png";
 
 export default function RootLayout({
-    children,
-  }: Readonly<{
-    children: React.ReactNode;
-  }>) {
-    return (
-      <main
-      className="px-8 py-4 w-full flex flex-col items-center justify-center text-center bg-green-900"
-    >
-                <p className=" p-8 font-inter  font-bold text-paragraph-md text-black-100 ">
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <main className=" bg-secondary-500 bg-cover bg-center min-h-screen flex flex-col ">
+      <Link className="flex items-center gap-2 mx-auto pt-16 pb-8" href={"/"}>
+        <Image src={logo} alt="Agrinet logo" className="w-10 h-[36px]" />
+        <p className="font-poppins text-heading-desktop-h4 font-semibold text-left text-white-50">
           AgriNet
-        </p> 
-      <div className=" text-center bg-white-50 rounded-[8px]">
-        <div className="mx-[182px]  p-16">
-        {children}  
-        </div>
-      </div>
+        </p>
+      </Link>
+      <div>
+        {children}
+      </div>  
     </main>
-    );
-  }
-  
+  );
+}
